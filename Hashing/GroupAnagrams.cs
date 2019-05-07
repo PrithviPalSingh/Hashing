4fafa1334483c2ac993a180a27113a4e8b95ea2f
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2019-05-07 16:48:13 +0800

    Add project files.

---
+++ Hashing/GroupAnagrams.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    class GroupAnagrams
+    {
+        public static void fnCountSystem(string[] strArray)
+        {
+            Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                var word = strArray[i];
+                char[] charArr = word.ToCharArray();
+                Array.Sort(charArr);
+                string newWord = new string(charArr);
+                if (!dict.ContainsKey(newWord))
+                {
+                    dict.Add(newWord, new List<string>() { word });
+                }
+                else
+                {
+                    var list = dict[newWord];
+                    list.Add(word);
+                    dict[newWord] = list;
+                }
+            }
+
+            foreach (var item in dict.OrderBy(i => i.Value.Count))
+            {
+                Console.Write(item.Value.Count + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Hashing/LargestFibonacciSubsequence.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hashing
+{
+    class LargestFibonacciSubsequence
+    {
+        public static void fnFiboSubsequence(int[] arr)
+        {
+            StringBuilder sb = new StringBuilder();
+            int max = arr.Max();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            CreateFiboSeries(max, ref dict);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dict.ContainsKey(arr[i]))
+                    sb.Append(arr[i] + " ");
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static void CreateFiboSeries(int max, ref Dictionary<int, int> dict)
+        {
+            dict = new Dictionary<int, int>();
+            int a = 0;
+            int b = 1;
+            int c = a + b;
+            dict.Add(a, 1);
+            dict.Add(b, 1);
+            while (c <= max)
+            {
+                c = a + b;
+                if (!dict.ContainsKey(c))
+                    dict.Add(c, 1);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
